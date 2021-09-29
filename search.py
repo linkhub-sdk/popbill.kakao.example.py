6737d1544b8a16277877e2f5ffe5a8b11126a28f
ProWON <pro_won45@naver.com>
2021-09-29 16:23:31 +0900

    added comment search 조회기간 단위

--- search.py
+++ search.py
@@ -20,7 +20,8 @@ kakaoService.UseStaticIP = testValue.UseStaticIP
 kakaoService.UseLocalTimeYN = testValue.UseLocalTimeYN
 
 '''
-검색조건에 해당하는 카카오톡 전송내역 목록을 조회합니다. (최대 검색기간 : 6개월 이내)
+검색조건에 해당하는 카카오톡 전송내역 목록을 조회합니다. (조회기간 단위 : 최대 2개월)
+- 카카오톡 접수일시로부터 6개월 이내 접수건만 조회할 수 있습니다.
 - https://docs.popbill.com/kakao/python/api#Search
 '''
 try:
